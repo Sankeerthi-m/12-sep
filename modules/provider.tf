84b0fd4c242c830e402b708ff0b4f8d4b08bc3de
sankeerthi <sankeerthimadeti@gmail.com>
2024-09-16 09:01:17 +0530

    hi

--- modules/provider.tf
+++ modules/provider.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source = "hashicorp/aws"
+      version = "5.67.0"
     }
   }
 }
 
-#configure the aws provider 
 provider "aws" {
   region = "ap-south-1"
 access_key ="AKIAYWNKWEIUDVGQHIN6" 
